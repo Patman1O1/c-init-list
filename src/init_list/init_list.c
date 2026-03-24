796c9cd98104438833d17f5e189fc21f0978bfbc
Patman1O1 <patrickthomas101x@gmail.com>
2026-03-24 13:47:51 -0500

    Implement init_list

--- src/init_list/init_list.c
+++ src/init_list/init_list.c
@@ -1,2 +1,12 @@
 #include <init_list/init_list.h>
 
+void init_list_free(struct init_list* init_list_p)  {
+    if (init_list_p == nullptr) {
+        errno = EFAULT;
+        return;
+    }
+
+    free(init_list_p->values_p);
+    init_list_p->values_p = nullptr;
+    init_list_p->size = 0;
+}
