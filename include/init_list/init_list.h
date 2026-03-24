dc676fa3a52c580320312a79431f3cf4bb8ff7ad
Patman1O1 <patrickthomas101x@gmail.com>
2026-03-24 13:47:33 -0500

    Implement init_list

--- include/init_list/init_list.h
+++ include/init_list/init_list.h
@@ -1,9 +1,73 @@
 #ifndef INIT_LIST_H
 #define INIT_LIST_H
 
+#include <stdlib.h>
+#include <assert.h>
+#include <errno.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif // #ifdef __cplusplus
+
+#define init_list_init(type, init_list, init_list_size, ...)                    \
+    init_list.size = init_list_size;                                            \
+    init_list.values_p = (void*)calloc(init_list.size, sizeof(type));           \
+    if (init_list.values_p == nullptr) {                                        \
+        exit(errno);                                                            \
+    }                                                                           \
+                                                                                \
+    if (init_list.size > 0) {                                                   \
+        type __values[] = {__VA_ARGS__};                                        \
+        const size_t __values_size = sizeof(__values) / sizeof(__values[0]);    \
+        if (__values_size <= init_list.size && __values_size > 0) {             \
+            memcpy(init_list.values_p, __values, sizeof(type) * __values_size); \
+        } else {                                                                \
+            free(init_list.values_p);                                           \
+            errno = E2BIG;                                                      \
+            init_list.values_p = nullptr;                                       \
+        }                                                                       \
+    } else {                                                                    \
+        init_list.values_p = nullptr;                                           \
+    }                                                                           \
+
+#ifdef __GNUC__
+#define init_list_init1(type, array_size, ...) ({                                 \
+    struct init_list __init_list;                                                 \
+    __init_list.size = array_size;                                                \
+    __init_list.values_p = (void*)calloc(__init_list.size, sizeof(type));         \
+    if (__init_list.values_p == NULL) {                                           \
+        exit(errno);                                                              \
+    }                                                                             \
+                                                                                  \
+    if (__init_list.size > 0) {                                                   \
+        type __values[] = {__VA_ARGS__};                                          \
+        const size_t __values_size = sizeof(__values) / sizeof(__values[0]);      \
+        if (__values_size <= __init_list.size && __values_size > 0) {             \
+            memcpy(__init_list.values_p, __values, sizeof(type) * __values_size); \
+        } else {                                                                  \
+            free(__init_list.values_p);                                           \
+            errno = E2BIG;                                                        \
+            __init_list.values_p = NULL;                                          \
+        }                                                                         \
+    } else {                                                                      \
+        __init_list.values_p = NULL;                                              \
+    }                                                                             \
+    __init_list;                                                                  \
+})
+
+#endif // #ifdef __GNUC__
+
+#define init_list_at(type, init_list, index) \
+    (assert(init_list.values_p != nullptr),  \
+     assert(index < init_list.size),         \
+    ((type*)init_list.values_p)[index])      \
+
+struct init_list {
+    void* values_p;
+    size_t size;
+};
+
+void init_list_free(struct init_list* init_list_p);
 
 #ifdef __cplusplus
 }
